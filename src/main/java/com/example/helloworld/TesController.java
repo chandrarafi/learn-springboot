e6b5e548010cf90432f9d7a172744959a693dfd7
Rafi Chandra <crafi2606@gmail.com>
2026-07-18 15:03:26 +0700

    setup

---
+++ src/main/java/com/example/helloworld/TesController.java
@@ -0,0 +1,7 @@
+package com.example.helloworld;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+public class TesController {
+    
+}
---
+++ src/main/java/com/example/helloworld/TestLombok.java
@@ -0,0 +1,7 @@
+package com.example.helloworld;
+import lombok.Data;
+
+@Data
+public class TestLombok {
+    private String name;
+}
